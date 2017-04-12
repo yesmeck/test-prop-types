e3a7f5a784bccda8e0ca9afc1a4e00801794b1c8
Wei Zhu <yesmeck@gmail.com>
2017-04-12 18:08:00 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import logo from './logo.svg';
+import './App.css';
+
+class Foo extends Component {
+  static propTypes = {
+    a: PropTypes.string.isRequired
+  }
+
+  render() {
+    return (
+      <div>Foo</div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Foo />
+      </div>
+    );
+  }
+}
+
+export default App;
